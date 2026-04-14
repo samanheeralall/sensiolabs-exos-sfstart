434fd0a2d7f15c855f64d6c08dd0fe92b3bfe298
Heeralall <heeralall@MacBook-Air-de-Heeralall.local>
2026-04-14 16:23:03 +0200

    Exercice 9

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+import 'bootstrap.php';
 import './stimulus_bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
@@ -5,6 +6,7 @@ import './stimulus_bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
